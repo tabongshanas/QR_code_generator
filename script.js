3f85905248314f88a5eb11d491556265b03c215a
shanasT <shanastabong@gmail.com>
2025-06-24 19:40:43 +0000

    finish with the creation of the QR code without responsive

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strick"
+
+const qrInput = document.getElementById('qr-input');
+// const darkMode = document.getElementById('dark-mode-con');
+// const brightMode = document.getElementById('bright-mode');
+const qrImg = document.querySelector('.qr-img');
+const geneBtn = document.querySelector('.gene-btn');
+const qrImgCon = document.querySelector('.qr-img-con');
+const slider = document.querySelector('.slider');
+const slide = document.querySelector('.slide');
+const container = document.querySelector('.container');
+const qrOperation = document.querySelector('.qr-operation');
+
+slider.classList.add('s-a');
+slide.classList.add('sl-a');
+
+geneBtn.addEventListener('click' , (e) => {
+    e.preventDefault(); 
+    
+    if (qrInput.value) {
+        qrImgCon.classList.add('qr-img-add'); 
+        qrInput.style = 'border: 1px solid rgba(0, 0, 0, 0.616)';
+        qrImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrInput.value;
+    }
+    else {
+        qrInput.style = 'border: 1px solid red';
+    }
+});
+
+
+slider.addEventListener('click' , (e) => {
+    e.preventDefault();
+    slider.classList.toggle('s-a');
+    slide.classList.toggle('sl-a');
+    slide.classList.toggle('slide-change');
+    container.classList.toggle('body'); 
+    qrOperation.classList.toggle('qroperate'); 
+});
